f121ff02bafbf4f8cdef22c8d26058216c24d9d4
Weelocks <72274796+Velox0@users.noreply.github.com>
2022-12-12 18:04:16 +0530

    Update autoupdate.bat

--- autoupdate.bat
+++ autoupdate.bat
@@ -1,6 +1,6 @@
 @echo off
 cd "%USERPROFILE%\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup"
-git clone https://github.com/pickleyoda/test.git
+git clone https://github.com/Velox0/test.git
 cd test
 git fetch origin main
 git merge origin/main
